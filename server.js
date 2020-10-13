8a877c5b2fe05d8b9b903c032aa34a7ff757fbb5
Oleksandr Yatsiuk <oleksandr.yatsiuk@gmail.com>
2020-10-13 19:57:17 +0300

    initial

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/Dominos'));
+app.use(express.static(__dirname + '/dist/lessons'));
 
 // Send all requests to index.html
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/Dominos/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/lessons/index.html'));
 });
 console.log(path.join(__dirname))
 
