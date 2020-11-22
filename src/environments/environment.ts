d179d247245ea5a6315308dd1ca9ac904ba51039
Oleksandr Yatsiuk <oleksandr.yatsiuk@gmail.com>
2020-11-22 23:38:05 +0200

    fix backend link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'https://lesson-backend.herokuapp.com/api/v1',
-  apiUrl: 'http://localhost:5000/api/v1',
+  apiUrl: 'https://lesson-backend.herokuapp.com/api/v1',
+  // apiUrl: 'http://localhost:5000/api/v1',
   telegaBotToken: '1370117694:AAFSUwYsNVwD-x-_vIFN23zqTG-P2q3dLPk'
 };
 
