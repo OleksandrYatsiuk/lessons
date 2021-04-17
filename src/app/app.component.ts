2a9440074abe3a8e34622a583dbe59ec08053eb2
Oleksandr Yatsiuk <oleksandr.yatsiuk@gmail.com>
2021-04-17 09:39:17 +0300

    set meta data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { isPlatformBrowser } from '@angular/common';
 import { ChangeDetectorRef, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Component } from '@angular/core';
+import { EnumLanguages, MetaDataService } from 'spm-core';
 import { PreloaderService } from './core/services/preloader.service';
 
 @Component({
@@ -15,12 +16,15 @@ export class AppComponent implements OnInit {
   constructor(
     @Inject(PLATFORM_ID) private platformId: any,
     private _ps: PreloaderService,
-    private _cd: ChangeDetectorRef
+    private _cd: ChangeDetectorRef,
+    private _md: MetaDataService
   ) {
     this.isBrowser = isPlatformBrowser(platformId);
   }
 
   ngOnInit(): void {
+    this._md.setSiteData('PLC', '/assets/logo.png', EnumLanguages.RUSSIAN);
+
     this._ps.loading
       .subscribe((isLoad: boolean) => {
         if (isLoad !== null) {
