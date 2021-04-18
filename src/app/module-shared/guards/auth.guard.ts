c4ac07d6bbbcf4e3c393b0198f0226daf76a7f4e
Oleksandr Yatsiuk <oleksandr.yatsiuk@gmail.com>
2021-04-18 16:36:58 +0300

    added is Browser checking

--- src/app/module-shared/guards/auth.guard.ts
+++ src/app/module-shared/guards/auth.guard.ts
@@ -1,29 +1,38 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { LoginComponent } from 'src/app/module-admin-panel/login/login.component';
 import { LocalStorageService } from 'src/app/core/services/local-storage.service';
 import { DialogService } from 'primeng/dynamicdialog';
+import { isPlatformBrowser } from '@angular/common';
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private storage: LocalStorageService, private router: Router, private _ds: DialogService) { }
+    constructor(
+        @Inject(PLATFORM_ID) private platformId: any,
+        private storage: LocalStorageService,
+        private router: Router,
+        private _ds: DialogService,
+    ) { }
     canActivate(): boolean {
-        if (!this.storage.getFromLocalStorage('plc_token')) {
-            const dialog = this._ds.open(LoginComponent, {
-                header: 'Авторизація адміністратора',
-                styleClass: 'plc-dynamic-dialog',
-                dismissableMask: true
-            });
+        if (isPlatformBrowser(this.platformId)) {
+            if (!this.storage.getFromLocalStorage('plc_token')) {
+                const dialog = this._ds.open(LoginComponent, {
+                    header: 'Авторизація адміністратора',
+                    styleClass: 'plc-dynamic-dialog',
+                    dismissableMask: true
+                });
 
-            dialog.onClose.subscribe(result => {
-                if (result) {
-                    this.router.navigate(['/admin/users']);
-                    return true;
-                } else {
-                    this.router.navigate(['/']);
-                    return false;
-                }
-            });
+                dialog.onClose.subscribe(result => {
+                    if (result) {
+                        this.router.navigate(['/admin/users']);
+                        return true;
+                    } else {
+                        this.router.navigate(['/']);
+                        return false;
+                    }
+                });
+            }
         }
+
         return true;
     }
 }
